16c48b709c4cf16d52d158b4cba3ac6e1426c371
Artsiom <34836796+ArtsiomBarodka@users.noreply.github.com>
2020-01-26 20:25:12 +0300

    Add files via upload

---
+++ src/main/java/by/javatr/controller/command/Command.java
@@ -0,0 +1,9 @@
+package by.javatr.ibank.controller.command;
+
+/**
+ * @author Barodka Artsiom
+ * @see https://github.com/ArtsiomBarodka
+ */
+public interface Command {
+    String execute(String request);
+}
