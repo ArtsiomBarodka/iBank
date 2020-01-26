ef8134d66b46e28eae5848d6786d5ed2f9bf57af
Artsiom <34836796+ArtsiomBarodka@users.noreply.github.com>
2020-01-26 20:39:13 +0300

    Add files via upload

---
+++ src/main/java/by/javatr/service/exception/ServiceSourceException.java
@@ -0,0 +1,11 @@
+package by.javatr.ibank.service.exception;
+
+/**
+ * @author Barodka Artsiom
+ * @see https://github.com/ArtsiomBarodka
+ */
+public class ServiceSourceException extends Exception{
+    public ServiceSourceException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
---
+++ src/main/java/by/javatr/service/exception/ServiceValidationException.java
@@ -0,0 +1,11 @@
+package by.javatr.ibank.service.exception;
+
+/**
+ * @author Barodka Artsiom
+ * @see https://github.com/ArtsiomBarodka
+ */
+public class ServiceValidationException extends Exception{
+    public ServiceValidationException(String message) {
+        super(message);
+    }
+}
