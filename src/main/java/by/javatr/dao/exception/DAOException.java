88db432503901c400e0a2d760c50442990b84011
Artsiom <34836796+ArtsiomBarodka@users.noreply.github.com>
2020-01-26 20:33:24 +0300

    Add files via upload

---
+++ src/main/java/by/javatr/dao/exception/DAOException.java
@@ -0,0 +1,11 @@
+package by.javatr.ibank.dao.exception;
+
+/**
+ * @author Barodka Artsiom
+ * @see https://github.com/ArtsiomBarodka
+ */
+public class DAOException extends Exception {
+    public DAOException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
